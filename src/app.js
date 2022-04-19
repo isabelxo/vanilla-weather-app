14445b377ac1cd052424ddda35e7b6bdefd7d0ba
isabelxo <97417409+isabelxo@users.noreply.github.com>
2022-04-19 18:15:30 -0500

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ function displayTemp(response) {
   let humidityDescriptionElement = document.querySelector("#humidity-weather");
   let windDescriptionElement = document.querySelector("#wind-weather");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
@@ -48,10 +49,15 @@ function displayTemp(response) {
   humidityDescriptionElement.innerHTML = response.data.main.humidity;
   windDescriptionElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = getDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "84919b6ce50d5f3343257ed5591f46ea";
-let city = "Pueblo";
+let city = "Colorado Springs";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemp);
